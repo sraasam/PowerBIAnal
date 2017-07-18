d3e37471dc18682d92e2b6e1d6e70cf815890bfd
Sai C. Raasam <sraasam@v2softinc.com>
2017-07-18 06:36:40 -0400

    First Commit

---
+++ RyderPortalApp/app.js
@@ -0,0 +1,16 @@
+var express = require('express');  
+var app = new express();  
+var port = 3000;  
+app.listen(port, function(err) {  
+    if (typeof(err) == "undefined") {  
+        console.log('Your application is running on : ' + port + ' port');  
+    }  
+});  
+app.use(express.static('public')); //making public directory as static diectory   
+app.use(express.static('src/views')); //making public directory as static diectory   
+app.get('/', function(req, res) {  
+    res.send('<h1>Hello C# Corner.</h1>');  
+});  
+app.get('/articles', function(req, res) {  
+    res.send('<h1>Welcome to C# Corner Articles.</h1>');  
+});  
